dacea964c6797cc2c17147371636d6483cd3e7b3
HectorMRC <thehector1593@gmail.com>
2025-12-22 19:28:14 +0100

    chore: enable experimental features

--- nixos/device.nix
+++ nixos/device.nix
@@ -38,7 +38,7 @@
     # Enable the OpenSSH daemon.
     services.openssh.enable = true;
 
-    # Nix optimizations.
+    # Nix configuration.
     nix = {
       gc = {
         automatic = true;
@@ -47,6 +47,11 @@
       };
 
       optimise.automatic = true;
+
+      settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
     };
 
     # This value determines the NixOS release from which the default
