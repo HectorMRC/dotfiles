66977e43bfce1def5d520da8cf490d31a5582d5c
HectorMRC <hector.morales@veecle.io>
2026-03-10 22:58:44 +0100

    chore: add DOCKER_HOST session env var

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,9 +1,21 @@
-{ name, ... }:
+{
+  name,
+  lib,
+  osConfig,
+  ...
+}:
 {
   programs.home-manager.enable = true;
 
   home.username = name;
   home.homeDirectory = "/home/${name}";
+
+  home.sessionVariables = lib.mkMerge [
+    (lib.mkIf (osConfig.virtualisation.podman.enable) {
+      DOCKER_HOST = "unix:///run/user/\${UID}/podman/podman.sock";
+    })
+    { }
+  ];
 
   # The state version is required and should stay at the version you
   # originally installed.
