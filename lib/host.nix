a86c272bdbb2f91a7dcd8da3800870ce6cb5770a
HectorMRC <hector.morales@veecle.io>
2026-09-13 18:28:43 +0200

    feat: add lib/host.nix mkHost helper

---
+++ lib/host.nix
@@ -0,0 +1,102 @@
+# Builds a Colmena node from a per-device description.
+
+let
+  nixos = ../nixos;
+  home = ../home-manager;
+  hardware = ../hardware-configuration;
+
+  desktopTags = [
+    "home"
+    "work"
+  ];
+
+  hasAny = wanted: tags: builtins.any (t: builtins.elem t tags) wanted;
+
+  baseNixosImports = [
+    (nixos + "/device.nix")
+    (nixos + "/locale.nix")
+    (nixos + "/network.nix")
+    (nixos + "/startup.nix")
+  ];
+
+  desktopNixosImports = [
+    (nixos + "/bluetooth.nix")
+    (nixos + "/pam.nix")
+    (nixos + "/pipewire.nix")
+    (nixos + "/desktop")
+  ];
+
+  baseHomeImports = [
+    home
+    (home + "/direnv.nix")
+    (home + "/keygen.nix")
+    (home + "/neovim.nix")
+    (home + "/ssh.nix")
+    (home + "/theme.nix")
+    (home + "/tmux.nix")
+    (home + "/vcs.nix")
+    (home + "/zsh.nix")
+  ];
+
+  desktopHomeImports = [
+    (home + "/latex.nix")
+    (home + "/desktop")
+  ];
+in
+{
+  hostname,
+  username ? "hector",
+  tags,
+  allowLocalDeployment ? true,
+  knownHosts,
+  vcsUser,
+  wallpaper ? null,
+  profile ? "personal",
+  extraImports ? [ ],
+  extraHomeImports ? [ ],
+  extraConfig ? { },
+  extraHomeConfig ? { },
+}:
+let
+  isDesktop = hasAny desktopTags tags;
+  isWork = builtins.elem "work" tags;
+
+  nixosImports =
+    [ (hardware + "/${hostname}.nix") ]
+    ++ baseNixosImports
+    ++ (if isDesktop then desktopNixosImports else [ ])
+    ++ (if isWork then [ (nixos + "/virtualisation.nix") ] else [ ])
+    ++ extraImports;
+
+  homeImports =
+    baseHomeImports ++ (if isDesktop then desktopHomeImports else [ ]) ++ extraHomeImports;
+in
+{
+  deployment = {
+    inherit tags allowLocalDeployment;
+    targetHost = hostname;
+  };
+
+  imports = nixosImports;
+
+  profile = {
+    inherit hostname username;
+    shell = "zsh";
+  };
+
+  home-manager.users.${username} = {
+    imports = homeImports;
+
+    theme.name = "gruvbox";
+
+    knownHosts = builtins.attrValues knownHosts;
+
+    version-control-system = {
+      user = vcsUser;
+      extraTools = [ "jj" ];
+    };
+  }
+  // (if isDesktop then { desktopEnvironment = { inherit wallpaper profile; }; } else { })
+  // extraHomeConfig;
+}
+// extraConfig
